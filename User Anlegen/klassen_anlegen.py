2b8128bf14a16bff8d944d69c4e18eed004cecc0
martin <martin@martin>
2024-01-22 17:03:50 +0100

    lehrer und seminar implementiert

--- User Anlegen/klassen_anlegen.py
+++ User Anlegen/klassen_anlegen.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import sys
+import openpyxl
 
 logger = logging.getLogger(__name__)
 handler = logging.handlers.RotatingFileHandler("create_class.log", maxBytes=10000, backupCount=5)
@@ -12,6 +13,43 @@ stream_handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 logger.addHandler(stream_handler)
+
+def create_delete_user(name, create, delete):
+    return None
+
+
+def create_credentials():
+    logger.info("Creating credentials sheet")
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+
+    sheet["A1"] = "Username"
+    sheet["B1"] = "Password"
+    return workbook, sheet
+
+
+def add_credential(sheet, name, pwd, row):
+    logger.info(f"Password for {name} added")
+    sheet[f"A{row}"] = name
+    sheet[f"B{row}"] = pwd
+
+def create_skripts(file):
+    logger.info("Skriptgenerierung startet")
+
+    cred_wb, cred_sh = create_credentials()
+
+    with open("create_user.sh", "w") as create, open("delete_user.sh", "w") as delete:
+        print("#!/bin/bash", file=create)
+        print("set -e", file=create)
+        print("#!/bin/bash", file=delete)
+        print("set -e", file=delete)
+
+        print("mkdir /home/klassen", file=create)
+
+        create_delete_user("lehrer", create, delete)
+        add_credential(cred_sh, "lehrer", 2, "lehrer")
+        create_delete_user("seminar", create, delete)
+        add_credential(cred_sh, "seminar", 3, "seminar")
 
 def start_logging(verbose, quiet):
     if quiet:
