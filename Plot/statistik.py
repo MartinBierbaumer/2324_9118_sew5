2563ea02262eb78363c3661efbb6840d9282b1f6
martin <martin@martin>
2024-02-12 12:43:48 +0100

    Statistik fertigestellt

--- Plot/statistik.py
+++ Plot/statistik.py
@@ -1,7 +1,8 @@
 import argparse
 import math
-from collections import defaultdict, Counter
+from collections import Counter
 from subprocess import Popen, PIPE
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="calculate number of ways through a labyrinth")
 parser.add_argument("directory", help="directory containing git repository")
@@ -12,11 +13,29 @@ git_log = ["git", "log", "--pretty=format:%ad", "--date=format-local:%a-%H-%M"]
 process = Popen(git_log, stdout=PIPE, stderr=PIPE, text=True)
 out, err = process.communicate()
 
-punkte = Counter()
+amount = Counter()
 commits = 0
 for date in out.splitlines():
     split = date.split("-")
-    punkte[(split[0], math.floor((int(split[1]) + int(split[2]) / 60) * 2))] += 1
+    amount[(split[0], math.floor((int(split[1]) + int(split[2]) / 60) * 2) / 2)] += 1
     commits += 1
 
-print(punkte)
+weekdays = ["", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su", ""]
+
+data = {"x": [], "y": [], "sizes": []}
+for day, time in amount:
+    data["x"].append(time)
+    data["y"].append(weekdays.index(day))
+    data["sizes"].append(100 + 25 * amount[(day, time)])
+
+plt.ylabel('Tag')
+plt.scatter(data['x'], data['y'], s=data['sizes'], alpha=0.5)
+plt.yticks(range(len(weekdays)), labels=weekdays)
+
+plt.xlabel('Zeit')
+plt.xticks(range(0, 25, 2))
+plt.title(f'Martin Bierbaumer: {commits} commits')
+plt.grid(True, which="major", axis="y", linestyle="-", linewidth=2, color='black')
+
+plt.savefig("statistic.png", dpi=72)
+plt.show()
