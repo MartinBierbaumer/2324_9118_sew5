95ace07d41a4434168d7386568b23a9df8ad2371
martin <martin@martin>
2024-02-12 12:26:06 +0100

    Commits gegroupt

--- Plot/statistik.py
+++ Plot/statistik.py
@@ -1,4 +1,6 @@
 import argparse
+import math
+from collections import defaultdict, Counter
 from subprocess import Popen, PIPE
 
 parser = argparse.ArgumentParser(description="calculate number of ways through a labyrinth")
@@ -10,3 +12,11 @@ git_log = ["git", "log", "--pretty=format:%ad", "--date=format-local:%a-%H-%M"]
 process = Popen(git_log, stdout=PIPE, stderr=PIPE, text=True)
 out, err = process.communicate()
 
+punkte = Counter()
+commits = 0
+for date in out.splitlines():
+    split = date.split("-")
+    punkte[(split[0], math.floor((int(split[1]) + int(split[2]) / 60) * 2))] += 1
+    commits += 1
+
+print(punkte)
